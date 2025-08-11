b419cd0393aff297b8fd2099807b5a358c5f3899
jnburchett <jnburchett@yahoo.com>
2025-08-11 11:00:10 -0600

    fix table read (ascii)

--- joebvp/atomicdata/atomicdata.py
+++ joebvp/atomicdata/atomicdata.py
@@ -15,7 +15,7 @@ jvp = importlib.util.find_spec('joebvp')
 jbvp_path = jvp.submodule_search_locations[0]
 # jbvp_path = imp.find_module('joebvp')[1]
 
-verntab = Table.read(jbvp_path+'/atomicdata/verner6_formatted.dat')
+verntab = Table.read(jbvp_path+'/atomicdata/verner6_formatted.dat', format = 'ascii')
 vernlam = verntab['lambda']
 vernion = verntab['ion']
 vernzatom = verntab['Z']
